10ec23679270c2f003bce5ae90daa2437280253c
itzelic <en3873@gmail.com>
2024-02-19 14:56:31 +0900

    #2 프로젝트 생성
    프로젝트 생성 완료

---
+++ src/main/java/com/open_skill/OpenSkillApplication.java
@@ -0,0 +1,13 @@
+package com.open_skill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSkillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open_skill/OpenSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open_skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSkillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
